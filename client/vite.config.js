bb329ef11f7a26b87ae08867bcde213a6cbd4904
Vlada Labza <vlada.labza@gmail.com>
2025-05-27 14:52:27 +0300

    fff

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import dotenv from 'dotenv';
 
 
-dotenv.config();
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
     tailwindcss(), 
   ],
-    define: {
-    __API__: JSON.stringify(process.env.VITE_API_URL), // можно использовать __API__ в коде
-  },
+
 
 })
